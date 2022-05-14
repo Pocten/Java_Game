659be10e1d3740660235fb96ec8fa96004257bd7
iastrnik <iastrnik@fel.cvut.cz>
2022-05-14 13:21:15 +0200

    Start-window

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import View.GameFrame;
+
+import java.awt.*;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+public class Main {
+    private static final Logger logger = Logger.getGlobal();
+
+    //start the game
+    public static void main(String[] args) throws IOException, AWTException {
+        logger.info("The game has begun");
+        GameFrame frame = new GameFrame();
+    }
+}
